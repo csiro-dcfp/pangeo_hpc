9a8f7c355d29f0615ccf2598a805be76df0b6405
dougiesquire <dougiesquire@gmail.com>
2020-09-12 15:50:22 +1000

    extending to Pearcey

--- start_jupyter_Gadi.sh
+++ start_jupyter_Gadi.sh
@@ -1,73 +1,21 @@
 #!/bin/bash -l
 
 #PBS -P v14
+#PBS -l storage=gdata/v14+scratch/v14
+
 #PBS -q normal
 #PBS -l walltime=02:00:00
 #PBS -l mem=16gb
 #PBS -l ncpus=4
 #PBS -l jobfs=100GB
 #PBS -l wd
-#PBS -l storage=gdata/v14+scratch/v14
 #PBS -j oe
-#PBS -o ~/
-#PBS -N jupyter
+#PBS -o .out/jupyter.%J.out 
 
 if [ ! $# -eq 0 ]; then
     NOTEBOOK_DIR=$1
 fi
+LOG_DIR=/g/data/${PROJECT}/${USER}/tmp/logs
+HPC_ADDRESS=gadi.nci.org.au
 
-# TO BE EDITED BY THE USER (MAYBE) 
-# ================================
-conda activate pangeo
-
-LOG_DIR=/g/data/v14/${USER}/tmp/logs
-# ================================
-
-HOST=$(hostname)
-mkdir -p $LOG_DIR
-LOGFILE=$LOG_DIR/pangeo_jupyter_log.$(date +%Y%m%dT%H%M%S)
-INSTRUCTIONSFILE=./jupyter_instructions.txt
-rm -f $INSTRUCTIONSFILE
-
-# Start the notebook -----
-echo -e "Starting jupyter notebook..." \
-> $INSTRUCTIONSFILE
-JPORT=$(shuf -n 1 -i 8301-8400) # As in NCI pangeo module
-jupyter lab --no-browser --ip=$HOST --port=${JPORT} --notebook-dir=$NOTEBOOK_DIR \
->& $LOGFILE &
-
-# Wait for notebook to start -----
-ELAPSED=0
-ADDRESS=
-while [[ $ADDRESS != *"${HOST}"* ]]; do
-    sleep 1
-    ELAPSED=$(($ELAPSED+1))
-    ADDRESS=$(grep -e '^\[.*\]\s*http://.*:.*/.*' $LOGFILE | head -n 1 | awk -F'//' '{print $NF}')
-    if [[ $ELAPSED -gt 360 ]]; then
-        echo -e "Something went wrong:\n-----"
-        cat $LOGFILE
-        echo "-----"
-        kill_server
-    fi
-done
-
-# Print jupyter port forwarding info -----
-PORT=$(echo $ADDRESS | awk -F':' ' { print $2 } ' | awk -F'/' ' { print $1 } ')
-TOKEN=$(echo $ADDRESS | awk -F'=' ' { print $NF } ')
-INSTRUCTIONS="
-Run the following command on your local computer:
-  ssh -N -L $PORT:${HOST}:$PORT ${USER}@gadi.nci.org.au
-Then open a browser and go to:
-  http://localhost:$PORT
-The Jupyter web interface will ask you for your password.
-
-To get the dashboard working, use the dask labextension and navigate to
-  http://localhost:$PORT/proxy/<dashboard-port>
-
-Use Control-C to shut down this job...
-"
-echo "$INSTRUCTIONS" >> $INSTRUCTIONSFILE
-
-# Wait for user kill command -----
-sleep inf
-
+./start_jupyter.sh ${NOTEBOOK_DIR} ${LOG_DIR} ${HPC_ADDRESS}
--- start_jupyter_Pearcey.sh
+++ start_jupyter_Pearcey.sh
@@ -3,64 +3,12 @@
 #SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=16gb
 #SBATCH --cpus-per-task=4
-#SBATCH --output=$LOG_DIR/jupyter-%J.out 
+#SBATCH --output=.out/jupyter.%a.out 
 
 if [ ! $# -eq 0 ]; then
     NOTEBOOK_DIR=$1
 fi
-
-# TO BE EDITED BY THE USER (MAYBE) 
-# ================================
-conda activate pangeo
-
 LOG_DIR=/scratch1/${USER}/tmp/logs
-# ================================
+HPC_ADDRESS=pearcey.hpc.csiro.au
 
-HOST=$(hostname)
-mkdir -p $LOG_DIR
-LOGFILE=$LOG_DIR/pangeo_jupyter_log.$(date +%Y%m%dT%H%M%S)
-INSTRUCTIONSFILE=./jupyter_instructions.txt
-rm -f $INSTRUCTIONSFILE
-
-# Start the notebook -----
-echo -e "Starting jupyter notebook..." \
-> $INSTRUCTIONSFILE
-JPORT=$(shuf -n 1 -i 8301-8400) # As in NCI pangeo module
-jupyter lab --no-browser --ip=$HOST --port=${JPORT} --notebook-dir=$NOTEBOOK_DIR \
->& $LOGFILE &
-
-# Wait for notebook to start -----
-ELAPSED=0
-ADDRESS=
-while [[ $ADDRESS != *"${HOST}"* ]]; do
-    sleep 1
-    ELAPSED=$(($ELAPSED+1))
-    ADDRESS=$(grep -e '^\[.*\]\s*http://.*:.*/.*' $LOGFILE | head -n 1 | awk -F'//' '{print $NF}')
-    if [[ $ELAPSED -gt 360 ]]; then
-        echo -e "Something went wrong:\n-----"
-        cat $LOGFILE
-        echo "-----"
-        kill_server
-    fi
-done
-
-# Print jupyter port forwarding info -----
-PORT=$(echo $ADDRESS | awk -F':' ' { print $2 } ' | awk -F'/' ' { print $1 } ')
-TOKEN=$(echo $ADDRESS | awk -F'=' ' { print $NF } ')
-INSTRUCTIONS="
-Run the following command on your local computer:
-  ssh -N -L $PORT:${HOST}:$PORT ${USER}@pearcey.hpc.csiro.au
-Then open a browser and go to:
-  http://localhost:$PORT
-The Jupyter web interface will ask you for your password.
-
-To get the dashboard working, use the dask labextension and navigate to
-  http://localhost:$PORT/proxy/<dashboard-port>
-
-Use Control-C to shut down this job...
-"
-echo "$INSTRUCTIONS" >> $INSTRUCTIONSFILE
-
-# Wait for user kill command -----
-sleep inf
-
+./start_jupyter.sh ${NOTEBOOK_DIR} ${LOG_DIR} ${HPC_ADDRESS}
