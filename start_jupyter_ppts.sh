8c7e1437e427f17235137a1ec3c9c604a6e56a85
dougiesquire <dougiesquire@gmail.com>
2021-06-01 12:52:31 +1000

    initial commit

---
+++ start_jupyter_ppts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=6G
+#SBATCH --cpus-per-task=4
+#SBATCH --output=/dev/null 
+
+# ====================================
+# Submit a job running Jupyter on ppts-login
+#
+#    USAGE: qsub --export NOTEBOOK_DIR=<directory to start notebook in> \
+#                --export RUN_SCRIPT_DIR=<location of start_jupyter.sh> \
+#                --export PANGEO_ENV_NAME=<name of pangeo environment> \
+#                start_jupyter_ppts.sh
+#
+#    Dougie Squire
+#    19/08/2020
+# ====================================
+
+if [ ! $# -eq 0 ]; then
+    NOTEBOOK_DIR=$1
+    RUN_SCRIPT_DIR=$2
+    PANGEO_ENV_NAME=$3
+fi
+
+# USER TO EDIT (optional)
+# ----------------------------
+LOG_DIR=/scratch1/${USER}/tmp/logs
+# ----------------------------
+
+HPC_ADDRESS=ppts-login.hpc.csiro.au
+
+${RUN_SCRIPT_DIR}/start_jupyter.sh ${NOTEBOOK_DIR} ${LOG_DIR} ${HPC_ADDRESS} ${PANGEO_ENV_NAME}
