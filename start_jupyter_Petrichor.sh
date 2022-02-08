34045cbd2edb7cef5298d898f30fbf4354aa8f68
dougiesquire <dougiesquire@gmail.com>
2022-02-08 12:47:16 +1100

    redcude Petrichor resources

--- start_jupyter_Petrichor.sh
+++ start_jupyter_Petrichor.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --time=02:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=64G
-#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=32G
+#SBATCH --cpus-per-task=4
 #SBATCH --output=/dev/null 
 
 # ====================================
